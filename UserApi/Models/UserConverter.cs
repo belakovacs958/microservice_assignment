e61ad38a4ae61d1d36ad866bec3c10b113fa774e
oliverbanizs <banizsoliver@gmail.com>
2022-10-19 18:15:44 +0200

    models

---
+++ UserApi/Models/UserConverter.cs
@@ -0,0 +1,35 @@
+﻿using SharedModels;
+
+namespace UserApi.Models
+{
+    public class UserConverter : IConverter<Customer, UserDto>
+    {
+        public Customer Convert(UserDto sharedUser)
+        {
+            return new Customer
+            {
+                Id = sharedUser.Id,
+                Name = sharedUser.Name,
+                Email = sharedUser.Email,
+                Phone = sharedUser.Phone,
+                BillingAddress = sharedUser.BillingAddress,
+                ShippingAddress = sharedUser.ShippingAddress,
+                CreditStanding = sharedUser.CreditStanding
+            };
+        }
+        public UserDto Convert(Customer hiddenUser)
+        {
+            return new UserDto
+            {
+                Id = hiddenUser.Id,
+                Name = hiddenUser.Name,
+                Email = hiddenUser.Email,
+                Phone = hiddenUser.Phone,
+                BillingAddress = hiddenUser.BillingAddress,
+                ShippingAddress = hiddenUser.ShippingAddress,
+                CreditStanding = hiddenUser.CreditStanding
+            };
+        }
+    }
+}
+
