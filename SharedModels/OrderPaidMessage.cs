df1289c6e0fff26142f93a37a75552219e8b5f8d
kendeszabo <szabo.kende20@gmail.com>
2022-10-26 19:11:05 +0200

    order paid pub sub functional

--- SharedModels/OrderPaidMessage.cs
+++ SharedModels/OrderPaidMessage.cs
@@ -1,8 +1,11 @@
-﻿namespace SharedModels
+﻿using System.Collections.Generic;
+
+namespace SharedModels
 {
     public class OrderPaidMessage
     {
         public int OrderId { get; set; }
         public int CustomerId { get; set; }
+        public IList<OrderLine> OrderLines { get; set; }
     }
 }
